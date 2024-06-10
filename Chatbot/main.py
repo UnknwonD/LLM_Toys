559d421b0b4550acbbd8ab9222f96f9d1982bfb6
UnknwonD <daeho5000@naver.com>
2024-06-11 06:58:08 +0900

    Ko LLM

---
+++ Chatbot/main.py
@@ -0,0 +1,46 @@
+
+import os
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+model_id = 'MLP-KTLim/llama-3-Korean-Bllossom-8B'
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=torch.bfloat16,
+    device_map="auto",
+)
+
+model.eval()
+
+PROMPT = '''당신은 유용한 AI 어시스턴트입니다. 사용자의 질의에 대해 친절하고 정확하게 답변해야 합니다.
+You are a helpful AI assistant, you'll need to answer users' queries in a friendly and accurate manner.'''
+instruction = "아주대학교에 대해 소개해줘"
+
+messages = [
+    {"role": "system", "content": f"{PROMPT}"},
+    {"role": "user", "content": f"{instruction}"}
+    ]
+
+input_ids = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt=True,
+    return_tensors="pt"
+).to(model.device)
+
+terminators = [
+    tokenizer.eos_token_id,
+    tokenizer.convert_tokens_to_ids("<|eot_id|>")
+]
+
+outputs = model.generate(
+    input_ids,
+    max_new_tokens=2048,
+    eos_token_id=terminators,
+    do_sample=True,
+    temperature=0.6,
+    top_p=0.9
+)
+
+print(tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True))
